04cd93dfb8c76340e6ba3ca948dca37cd1ab05bb
David <david.vitrier@gmail.com>
2026-07-11 21:17:32 +0200

    Ajoute le support PWA (manifest, icones, service worker) et agrandit les cases du manche pour le tactile
    Rend l'appli installable sur Android/PC depuis Chrome et utilisable au doigt.

---
+++ service-worker.js
@@ -0,0 +1,39 @@
+var CACHE_NAME = "guitare-theorie-v1";
+var CORE_FILES = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./manifest.json",
+  "./js/music-theory.js",
+  "./js/nav.js",
+  "./js/notes.js",
+  "./js/intervals.js",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", function(event){
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache){
+      return cache.addAll(CORE_FILES);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", function(event){
+  event.waitUntil(
+    caches.keys().then(function(names){
+      return Promise.all(names.filter(function(n){return n!==CACHE_NAME;}).map(function(n){return caches.delete(n);}));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", function(event){
+  event.respondWith(
+    caches.match(event.request).then(function(cached){
+      return cached || fetch(event.request);
+    })
+  );
+});
